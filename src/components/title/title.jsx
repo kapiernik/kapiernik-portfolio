6c068a6bb59f1abbeea057dc504e2731b097abfa
Mykola Kapiernik <75549767+kapiernik@users.noreply.github.com>
2021-07-16 23:07:34 +0300

    Added Title component

---
+++ src/components/title/title.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Title = styled.h2`
+    grid-area: title;
+    text-align: left;
+    margin-left: 50px;
+    margin-top: 50px;
+    font-size: 4rem;
+    font-weight: 600;
+`;
+
+export default Title;
