5f3b9a4bea441ca403489939cb582332939f7100
Mykola Kapiernik <75549767+kapiernik@users.noreply.github.com>
2021-07-16 22:15:10 +0300

    added AboutMe into App

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../header';
+import AboutMe from '../aboutMe';
 
 import './App.scss';
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
     </div>
   );
 }
