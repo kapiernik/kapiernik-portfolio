01b0cb554968f832806bf23b539c72fed1cbd2c1
Mykola Kapiernik <75549767+kapiernik@users.noreply.github.com>
2021-07-17 03:11:53 +0300

    added Skills into App

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../header';
 import AboutMe from '../aboutMe';
+import Skills from '../skills';
 
 import './App.scss';
 
@@ -9,6 +10,7 @@ const App = () => {
     <div className="App">
       <Header />
       <AboutMe />
+      <Skills />
     </div>
   );
 }
